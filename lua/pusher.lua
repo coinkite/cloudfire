3b750d6762480c84cd82151bf8f5cb2504a2d9f6
user <user>
2015-01-23 16:28:11 -0500

    MMM

--- lua/pusher.lua
+++ lua/pusher.lua
@@ -1,12 +1,20 @@
 -- web socket handler
 -- see https://github.com/openresty/lua-resty-redis/issues/46 near end
 
+-- validate channel string, because it's unclean
+local subchan = ngx.var.query_string or 'none'
+local cleaned, n, err = ngx.re.sub(subchan, '^([a-z0-9A-Z]{1,100})$', '$1')
+if cleaned ~= subchan or n ~= 1 or err then
+	LOG("got chan: " .. subchan .. " => " .. cleaned)
+	return ngx.exit(404)
+end
+
 local MAX_WS_PER_FID = 3
 local server = require "resty.websocket.server"
 local RDB = get_RDB()
 
 local wb, err = server:new({
-	timeout = 1000,  				-- milliseconds
+	timeout = 0,  				-- milliseconds
 	max_payload_len = 4095,
 })
 if not wb then
@@ -21,7 +29,7 @@ local wsid = pick_token()
 
 local function die(msg, fid, wsid)
 	-- connection is over.
-	LOG(wsid .. ": WS die: " .. msg)
+	LOG(wsid .. ": dies: " .. msg)
 	wb:send_close()
 
 	-- must unsubscribe first! Can't do redis otherwise.
@@ -41,14 +49,6 @@ if count and count > MAX_WS_PER_FID then
 	return die("too many", fid, wsid)
 end
 
--- validate channel string, because it's unclean
-local subchan = ngx.var.query_string or 'none'
-local cleaned, n, err = ngx.re.sub(subchan, '^([a-z0-9A-Z]{1,100})$', '$1')
-if cleaned ~= subchan or n ~= 1 or err then
-	LOG("got chan: " .. subchan .. " => " .. cleaned)
-	return die("bad chan", fid, wsid)
-end
-
 -- document our status
 local status = { wsid = wsid, fid = ngx.ctx.ACTIVE_FID, chan=subchan }
 RDB:sadd('sockets', wsid)
@@ -58,19 +58,22 @@ wb:send_text(cjson.encode(status))
 
 -- see https://github.com/openresty/lua-resty-websocket/issues/1#issuecomment-24816008
 local function client_rx(wb, fid, wsid)
-	LOG("listen on ", wsid)
 	local RDB = get_RDB()
 
 	wb:set_timeout(0)
 	while true do
 		local bytes, typ, err = wb:recv_frame()
-		if wb.fatal or err then
-			LOG("rx err: ", err)
-			return die("ws error", fid, wsid)
+		if wb.fatal then
+			-- comes here if they reload the page, close laptop, whatever.
+			return die("rx fatal", fid, wsid)
+		elseif err then
+			-- for timeouts.
+			LOG("err=" .. err)
+			return die("rx error", fid, wsid)
 		elseif typ == "close" then
 			return die("client close", fid, wsid)
 		elseif typ == "text" then
-			LOG("rx=" ..  bytes)
+			-- LOG(wsid .. ": RX: " ..  bytes)
 			RDB:rpush("websocket_rx", cjson.encode({wsid=wsid, msg=bytes}))
 		end
 	end
@@ -95,39 +98,32 @@ while true do
 	-- read and wait for next event on subscription channel
 	local res, err = RDB:read_reply()
 	if not res then
-		return die('read sub: ' .. err, fid, wsid)
-	end
+		if err == 'timeout' then
+			wb:send_ping('')
+		else
+			return die('read sub: ' .. err, fid, wsid)
+		end
+	else
 
- 	local mtype = res[1]
- 	local chan = res[2]
- 	local msg = res[3]
-	LOG("Got msg: " .. mtype)
+		local mtype = res[1]
+		local chan = res[2]
+		local msg = res[3]
 
- 	if mtype == "message" then
- 		-- normal traffic on the channel; copy to websocket
- 		local ok, err = wb:send_text(msg)
- 		if not ok then
- 			die("Couldn't write: " .. err, fid, wsid)
+		if mtype == "message" then
+			-- normal traffic on the channel; copy to websocket
+			if msg == 'PING' then
+				wb:send_ping('hi')
+			else
+				local ok, err = wb:send_text(cjson.encode({chan=chan, msg=msg}))
+				if not ok then
+					die("Couldn't write: " .. err, fid, wsid)
+				end
+			end
+					
+		elseif mtype == "unsubscribe" then
+			break
 		end
 	end
-			
---x-- 	elseif mtype == "unsubscribe" then
---x-- 		break
---x-- 	end
 end
 
-
-wb:send_text("First msg")
-wb:send_text("2nd msg")
-while true do
- 	wb:send_text("hello world")
- 	ngx.sleep(5)
-end
-
-local data, typ, err = wb:recv_frame()
-
-if not data then
-	ngx.log(ngx.ERR, "failed to receive a frame: ", err)
-	return ngx.exit(444)
-end
 
